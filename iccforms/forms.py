b4a4a89f4adb412f53c5a38277791922d6499ce8
Naveenfascinate <30978039+Naveenfascinate@users.noreply.github.com>
2021-02-01 12:49:33 +0530

    code cleaned

--- iccforms/forms.py
+++ iccforms/forms.py
@@ -38,54 +38,57 @@ def homeSubmit():
 
         return "success"
 
-
+# Register page
 @forms.route('/register',methods=['POST', 'GET'])
 def registerpage():
 
     global jsonFormData
+
+    # Getting data using POST method
     if request.method == 'POST':
         columnNames = []
         Data = request.form.getlist("getdata[]")
-        print(Data)
         formName = Data[1]
-        print("register")
+
+        # Connecting sqlLite Database
         con = sqlite3.connect('formdb.db')
         cursor = con.cursor()
+
+        # Fetching data from database
         selectCmd = "SELECT form_data,response_data FROM userforms WHERE form_name = '"+formName+"'"
         tabelData = cursor.execute(selectCmd)
-        print(tabelData)
+
+        # Converting data into Json Format
         for i in tabelData:
-            print(i[0])
             jsonData = json.dumps(i[0])
             jsonFormData = json.loads(jsonData)
-            print(type(jsonFormData))
-
-
-
-
 
         return ({'formData':jsonFormData})
     return render_template('formRegisteration.html')
 
-
+# Register page submit functionality
 @forms.route("/submit", methods=['POST', 'GET'])
 def submit():
+    # Getting data using POST method
     if request.method == 'POST':
         result ={}
+
+        # Connecting sqlLite Database
         con = sqlite3.connect('formdb.db')
         cursor = con.cursor()
 
+        # Getting data from client side
         data = request.form.get("resultdata")
         data = ast.literal_eval(data)
         questions = request.form.getlist("question[]")
         questionTypes = request.form.getlist("questionType[]")
         formName = request.form.get("getdata")
-        print(formName)
+
+        # Fetching data from database
         selectCmd = "SELECT response_data FROM userforms WHERE form_name = '" + formName + "'"
         tabelData = cursor.execute(selectCmd)
-        print(data)
+
         for i in tabelData:
-            print(i[0])
             if i[0] == None:
                 for j in range(len(questions)):
                     result[questions[j]]=[data[j]]
@@ -94,39 +97,37 @@ def submit():
                 result = i[0].replace("\'", "\"")
                 result = json.loads(result)
                 for j in range(len(questions)):
-
                     result[questions[j]].append(data[j])
 
-        print(result)
+        # Updating registered data in database
         tabelcmd = 'UPDATE userforms SET response_data = "'+str(result)+'" WHERE form_name = "'+formName+'"'
-        print(tabelcmd)
         cursor.execute(tabelcmd)
         con.commit()
-        return ("hi")
+        return ("success")
 
+# Enquiry page
 @forms.route("/enquiries", methods=['POST', 'GET'])
 def enquiry():
+    # Getting data using POST method
     if request.method == 'POST':
-        columnNames = []
+
+        # Getting form name
         Data = request.form.getlist("getdata[]")
-        print(Data)
         formName = Data[1]
-        print("register")
+
+        # Connecting sqlLite Database
         con = sqlite3.connect('formdb.db')
         cursor = con.cursor()
+
+        # Fetching data from database
         selectCmd = "SELECT response_data FROM userforms WHERE form_name = '"+formName+"'"
         tabelData = cursor.execute(selectCmd)
-        print(tabelData)
+
         for i in tabelData:
-            print(i[0])
             jsonData = json.dumps(i[0])
             jsonFormData = json.loads(jsonData)
-            print(type(jsonFormData))
 
-
-
-
-
+        # Converting data into Json Format
         return ({'formData':jsonFormData})
     return render_template('enquiries.html')
 
